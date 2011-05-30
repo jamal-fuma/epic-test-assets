ff6b726cf008c7eefab12f14c812bff2dfd3b796
Jamal M. Natour <jamal.natour@fumasoftware.com>
2011-05-30 05:14:21 +0100

    updated test script

--- tests/test-sample_input_53_of_501_lines.sh
+++ tests/test-sample_input_53_of_501_lines.sh
@@ -1,3 +1,4 @@
 cp input/input-sample_input_20_of_501_lines.csv     input.csv
-cp log/log-sample_input_20_of_501_lines.csv         log.log
-cp output/output-sample_input_20_of_501_lines.csv   output.csv
+cp log/log-sample_input_53_of_501_lines.csv         log.log
+cp output/output-sample_input_53_of_501_lines.csv   output.csv
+
