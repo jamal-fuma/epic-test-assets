e91ffe2634fb942043911f824ab748fd55e6b120
Jamal M. Natour <jamal.natour@fumasoftware.com>
2011-05-30 15:03:56 +0100

    added large test-file of historical data

---
+++ tests/test-sample_input_51_of_25352_lines.sh
@@ -0,0 +1,3 @@
+cp input/input-sample_input_51_of_25352_lines.csv     input.csv
+cp output/output-sample_input_51_of_25352_lines.csv   output.csv
+cp log/log-sample_input_51_of_25352_lines.log         log.log
