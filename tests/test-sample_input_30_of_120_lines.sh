1605d4d97f0862f41b178d46eb07531d5e9f3333
Jamal M. Natour <jamal.natour@fumasoftware.com>
2011-05-30 15:58:23 +0100

    added small correct test case

---
+++ tests/test-sample_input_30_of_120_lines.sh
@@ -0,0 +1,5 @@
+cp   input/input-sample_input_30_of_120_lines.csv   input.csv
+cp output/output-sample_input_30_of_120_lines.csv   output.csv
+cp       log/log-sample_input_30_of_120_lines.log   log.log
+
+#/home/me/workspace/clone/epic-food-frequency/build/epic_ffq -f _inst/etc/epic-food-frequency/conf/client.conf -i input/input-sample_input_30_of_120_lines.csv -o output/output-sample_input_30_of_120_lines.csv -l log/log-sample_input_30_of_120_lines.log  
